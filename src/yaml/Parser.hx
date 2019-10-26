f114d1cf055ed1e3d4080a2a86c0af5dfcbca5ff
GrepSuzette <emugel@qq.com>
2019-10-27 00:42:52 +0800

    forgive me

--- src/yaml/Parser.hx
+++ src/yaml/Parser.hx
@@ -2002,7 +2002,7 @@ class Parser
 	public static var PATTERN_NON_PRINTABLE         = ~/[\x{00}-\x{08}\x{0B}\x{0C}\x{0E}-\x{1F}\x{7F}-\x{84}\x{86}-\x{9F}\x{FFFE}\x{FFFF}]/u;
 	#elseif (js || flash9 || java)
 	public static var PATTERN_NON_PRINTABLE         = ~/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x84\x86-\x9F\uD800-\uDFFF\uFFFE\uFFFF]/u;	
-	#if (hl)
+	#elseif (hl)
 	public static var PATTERN_NON_PRINTABLE         = ~/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x84\x86-\x9F]/;	
 	#else
 	#error "Compilation target not supported due to lack of Unicode RegEx support."
