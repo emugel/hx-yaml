7743a9f09a4dd1a78ec45746fa1c82a69a7314bf
GrepSuzette <emugel@qq.com>
2019-10-27 12:31:45 +0800

    grmbl..

--- src/yaml/util/Utf8.hx
+++ src/yaml/util/Utf8.hx
@@ -1,16 +1,11 @@
 package yaml.util;
 
-#if false
-import haxe.Utf8;
-#else
-import yaml.Utf8;
-#end
 
 class Utf8
 {
 	public static function substring(value:String, startIndex:Int, ?endIndex:Null<Int>):String
 	{
-		var size = Utf8.length(value);
+		var size = yaml.Utf8.length(value);
 		var pos = startIndex;
 		var length = 0;
 		
@@ -28,6 +23,6 @@ class Utf8
 			length = endIndex - pos;
 		}
 		
-		return Utf8.sub(value, pos, length);
+		return yaml.Utf8.sub(value, pos, length);
 	}
 }
